4869c0a0af1b622b0ce404d1d7b7133c3750d3dc
matoo <matoogsm90@gmail.com>
2024-01-20 14:25:17 +0330

    fix: urls list in home uls and template

--- core/settings.py
+++ core/settings.py
@@ -123,9 +123,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media/'
 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('implementation.urls')),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
